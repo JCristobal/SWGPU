23de4a022a30cec881e3df707df2dd1bf201100a
JCristobal <tobas92@gmail.com>
2016-07-29 18:00:26 +0200

    Se crea el esqueleto para la petición web de cómputo usando la función Ackley

--- static/assets/js/controller.js
+++ static/assets/js/controller.js
@@ -2,14 +2,17 @@ var app = angular.module("myApp", []);
 
 
 app.controller("myController", function($scope) { 
-	
+
 	// Función para recoger el valor introducido por el usuario, hacer la consulta y devolver el resultado en #muestra_resultado
 	$scope.peticionCalculo = function(valor) { 		
 		// Modificamos el maquetado con jQuery y hacemos la petición con el valor introducido
 		$('#icono_espera').css('display','initial'); 
 		$('body').css('opacity','0.5'); 
-		$.get('/swgpu/peticion_datos/', {peticion: valor }, function(data){
-				// calcula la fecha actual para mostrarla por pantalla
+
+		var peticion_calculo = $.get('/swgpu/peticion_datos/', {peticion: valor });
+
+		peticion_calculo.success(function(data){
+		     // calcula la fecha actual para mostrarla por pantalla
 				var hora_pet = new Date();
 				$('#fecha_peticion').html(hora_pet);
 				// Dirige a los resultados, pero sin transición
@@ -33,7 +36,15 @@ app.controller("myController", function($scope) {
 				$('#muestra_datos').html(muestra_datos);
 		    	// Mostramos los resultados por pantalla
 		    	$('#muestra_resultado').html(JSON.stringify(datos.calculo.resultados));
-	    });	
+		});
+					 
+    };
+
+	$scope.valueA=20;
+	$scope.valueB=0.2;
+	$scope.valueC=2;
+
+    $scope.peticionAckley = function(a,b,c,valores) { 
 
     };
 	
