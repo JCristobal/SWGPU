28f76e8c0af46f841a0fa75e859acde7c7fd0806
JCristobal <tobas92@gmail.com>
2016-09-03 12:22:26 +0200

    Se ajusta el maquetado de varias secciones. También se actualiza el ejecutable con el algoritmo genético.

--- static/assets/js/controller.js
+++ static/assets/js/controller.js
@@ -3,6 +3,25 @@ var app = angular.module("myApp", []);
 
 app.controller("myController", function($scope, $http) { 
 
+	$scope.muestra_ackley = function(){
+		$('#serv_ackley').css('display','initial');
+		$('#serv_ackley').css('border-top','2px solid black');
+		$('.container').css('height','100vh');
+		$('#serv_rastrigin').css('display','none');
+
+		$('.fecha_peticion').html("");
+		$('#muestra_resultado').html("Lance un algoritmo para ver sus resultados");
+	}
+
+	$scope.muestra_rastrigin = function(){
+		$('#serv_rastrigin').css('display','initial');
+		$('.container').css('height','100vh');
+		$('#serv_ackley').css('display','none');
+
+		$('.fecha_peticion').html("");
+		$('#muestra_resultado').html("Lance un algoritmo para ver sus resultados");
+	}
+
 	$scope.valueA=20;
 	$scope.valueB=0.2;
 	$scope.valueC=2*Math.PI;
@@ -20,9 +39,10 @@ app.controller("myController", function($scope, $http) {
 
 
     $scope.peticionAckley = function(a, b, c, max_gen, min, max, p_mutation, p_crossover, population_size, n_vars) { 
-
+	
 		$('.icono_espera').css('display','initial'); 
 		$('body').css('opacity','0.5'); 
+		$('#muestra_resultado').html("Procesando el algoritmo, espere para ver los resultados");
 
 		var datos = {valorA: a, valorB: b, valorC: c, maxgen: max_gen, valorMin: min, valorMax: max, pMutation: p_mutation, pCrossover: p_crossover, populationSize: population_size, nVars: n_vars };
 
@@ -36,6 +56,8 @@ app.controller("myController", function($scope, $http) {
 
 		    // calcula la fecha actual para mostrarla por pantalla
 			var hora_pet = new Date();
+																
+			$('.fecha_peticion').html("<h3> Fecha de petición: </h3> <div id='fecha_peticion'>  </div>");
 			$('#fecha_peticion').html(hora_pet);
 			// Dirige a los resultados, pero sin transición
 			// //window.location="#2";
@@ -44,11 +66,16 @@ app.controller("myController", function($scope, $http) {
 			$('.icono_espera').css('display','none');
 			$('body').css('opacity','1');
 	
-			// Variable para mostrar las variables
-			var variables = "";
+
+			// Variable para mostrar las variables con una tabla
+			var variables = "<div class='table-wrapper'> <table> <thead><tr><th>Variable</th><th>Valor</th></tr></thead> <tbody>";
+
 			for(var i=0;i<n_vars;i++){
-				variables = variables +" Variable "+i+": "+ response.data.calculo.resultado_AG.values[i]+"<br>";
+				variables = variables +"<tr> <td>"+i+"</td> <td>"+ response.data.calculo.resultado_AG.values[i]+"</td></tr>";
 			}
+			variables = variables + "</tbody> </table> </div>";
+
+
 			// Variable para mostrarlo en forma de "array"
 			var listaVariables = "[";
 			for(var i=0;i<n_vars-1;i++){
@@ -66,13 +93,20 @@ app.controller("myController", function($scope, $http) {
 										+"<strong>Para la entrada: </strong> <br> Número de generaciones: "+JSON.stringify(response.data.calculo.info_input.n_generations).replace(/\"/g,"")+"<br>"
 										+"Valor mínimo: "+JSON.stringify(response.data.calculo.info_input.minimal_value).replace(/\"/g,"")+"<br>"
 										+"Valor máximo: "+JSON.stringify(response.data.calculo.info_input.maximum_value).replace(/\"/g,"")+"<br>"
-										+"Con :"+JSON.stringify(response.data.calculo.info_input.n_vars).replace(/\"/g,"")+" variables <br>"
+										+"Para "+JSON.stringify(response.data.calculo.info_input.n_vars).replace(/\"/g,"")+" variables (tamaño del cromosoma) <br>"
 										+"Probabilidad de mutación: "+(JSON.stringify(response.data.calculo.info_input.p_mutation).replace(/\"/g,""))*100+"% <br>"
 										+"Probabilidad de cruce: "+(JSON.stringify(response.data.calculo.info_input.p_crossover).replace(/\"/g,""))*100+"% <br>"
 										+"Y una población de "+JSON.stringify(response.data.calculo.info_input.population_size).replace(/\"/g,"")+" <br>"
 										+"Con las variables a: "+JSON.stringify(response.data.calculo.info_input.a).replace(/\"/g,"")+" b:"+JSON.stringify(response.data.calculo.info_input.b).replace(/\"/g,"")+" c:"+JSON.stringify(response.data.calculo.info_input.c).replace(/\"/g,"")+" propias de la función de optmización Ackley<br>"
-										+"<br>Tras "+JSON.stringify(response.data.calculo.resultado_AG.generations).replace(/\"/g,"")+" generaciones con el mejor 'fitness' de "+JSON.stringify(response.data.calculo.resultado_AG.best_fitness).replace(/\"/g,"")+" se obtienen las <strong>variables:</strong> <br>"+variables +"<br>"
+										+"<br>Tras "+JSON.stringify(response.data.calculo.resultado_AG.generations).replace(/\"/g,"")+" generaciones con el mejor 'fitness' de "+JSON.stringify(response.data.calculo.resultado_AG.best_fitness).replace(/\"/g,"")+" se obtienen como <strong>resultado:</strong> <br>"+variables +"<br>"
 										+"<strong>Array con las variables: </strong>"+listaVariables );
+
+
+			// Ya que se genera una tabla de resultados muy grande, ajustamos el tamaño de la sección
+			var resultados_alto = $("#muestra_resultado").height();
+			resultados_alto=resultados_alto+350;
+			resultados_alto=resultados_alto+"px";
+			$('#resultados').css('height',resultados_alto); 
 
 
 		  }, function errorCallback(response) {
@@ -89,6 +123,7 @@ app.controller("myController", function($scope, $http) {
 		
 		$('.icono_espera').css('display','initial'); 
 		$('body').css('opacity','0.5'); 
+		$('#muestra_resultado').html("Procesando el algoritmo, espere para ver los resultados");
 
 		var datos = {valorARastrigin: ar, maxgen: max_gen, valorMin: min, valorMax: max, pMutation: p_mutation, pCrossover: p_crossover, populationSize: population_size, nVars: n_vars };
 
@@ -102,6 +137,7 @@ app.controller("myController", function($scope, $http) {
 
 		    // calcula la fecha actual para mostrarla por pantalla
 			var hora_pet = new Date();
+			$('.fecha_peticion').html("<h3> Fecha de petición: </h3> <div id='fecha_peticion'>  </div>");
 			$('#fecha_peticion').html(hora_pet);
 			// Dirige a los resultados, pero sin transición
 			// //window.location="#2";
@@ -110,11 +146,16 @@ app.controller("myController", function($scope, $http) {
 			$('.icono_espera').css('display','none');
 			$('body').css('opacity','1');
 	
-			// Variable para mostrar las variables
-			var variables = "";
+
+			// Variable para mostrar las variables con una tabla
+			var variables = "<div class='table-wrapper'> <table> <thead><tr><th>Variable</th><th>Valor</th></tr></thead> <tbody>";
+
 			for(var i=0;i<n_vars;i++){
-				variables = variables +" Variable "+i+": "+ response.data.calculo.resultado_AG.values[i]+"<br>";
+				variables = variables +"<tr> <td>"+i+"</td> <td>"+ response.data.calculo.resultado_AG.values[i]+"</td></tr>";
 			}
+			variables = variables + "</tbody> </table> </div>";
+
+
 			// Variable para mostrarlo en forma de "array"
 			var listaVariables = "[";
 			for(var i=0;i<n_vars-1;i++){
@@ -132,13 +173,20 @@ app.controller("myController", function($scope, $http) {
 										+"<strong>Para la entrada: </strong> <br> Número de generaciones: "+JSON.stringify(response.data.calculo.info_input.n_generations).replace(/\"/g,"")+"<br>"
 										+"Valor mínimo: "+JSON.stringify(response.data.calculo.info_input.minimal_value).replace(/\"/g,"")+"<br>"
 										+"Valor máximo: "+JSON.stringify(response.data.calculo.info_input.maximum_value).replace(/\"/g,"")+"<br>"
-										+"Con "+JSON.stringify(response.data.calculo.info_input.n_vars).replace(/\"/g,"")+" variables <br>"
+										+"Para "+JSON.stringify(response.data.calculo.info_input.n_vars).replace(/\"/g,"")+" variables (tamaño del cromosoma)<br>"
 										+"Probabilidad de mutación: "+(JSON.stringify(response.data.calculo.info_input.p_mutation).replace(/\"/g,""))*100+"% <br>"
 										+"Probabilidad de cruce: "+(JSON.stringify(response.data.calculo.info_input.p_crossover).replace(/\"/g,""))*100+"% <br>"
 										+"Y una población de "+JSON.stringify(response.data.calculo.info_input.population_size).replace(/\"/g,"")+" <br>"
 										+"Con la variable A: "+JSON.stringify(response.data.calculo.info_input.A).replace(/\"/g,"")+" propia de la función de optmización Rastrigin<br>"
 										+"<br>Tras "+JSON.stringify(response.data.calculo.resultado_AG.generations).replace(/\"/g,"")+" generaciones con el mejor 'fitness' de "+JSON.stringify(response.data.calculo.resultado_AG.best_fitness).replace(/\"/g,"")+" se obtienen las <strong>variables:</strong> <br>"+variables +"<br>"
 										+"<strong>Array con las variables: </strong>"+listaVariables );
+
+
+			// Ya que se genera una tabla de resultados muy grande, ajustamos el tamaño de la sección
+			var resultados_alto = $("#muestra_resultado").height();
+			resultados_alto=resultados_alto+350;
+			resultados_alto=resultados_alto+"px";
+			$('#resultados').css('height',resultados_alto); 
 
 
 		  }, function errorCallback(response) {
